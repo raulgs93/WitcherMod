a2f2aef83ddb64753c55809763cf9b60c02cadf6
raulgs93 <raul.garcia.santiago93@gmail.com>
2020-02-09 23:27:05 +0100

    Test fire effect

--- src/main/java/com/musgo/witchermod/Effects/TestEffect.java
+++ src/main/java/com/musgo/witchermod/Effects/TestEffect.java
@@ -1,10 +1,14 @@
 package com.musgo.witchermod.Effects;
 
 
+import com.musgo.witchermod.Main;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
 import net.minecraft.potion.Effects;
 
+import javax.annotation.Nullable;
 import java.awt.*;
 
 
@@ -19,6 +23,20 @@ public class TestEffect extends Effect {
         this(effectType, new Color(liquidR, liquidG, liquidB).getRGB());
     }
 
-    
+    @Override
+    public void performEffect(LivingEntity entityLivingBaseIn, int amplifier) {
+        Main.LOGGER.info("Setting " + entityLivingBaseIn.getName().getFormattedText() + " on fire");
+        entityLivingBaseIn.setFire(3);
+    }
 
+    @Override
+    public boolean isInstant() {
+        return  true;
+    }
+
+    @Override
+    public void affectEntity(@Nullable Entity source, @Nullable Entity indirectSource, LivingEntity entityLivingBaseIn, int amplifier, double health) {
+        Main.LOGGER.info("Setting " + entityLivingBaseIn.getName().getFormattedText() + " on fire from affectEntity");
+        entityLivingBaseIn.setFire(3);
+    }
 }
